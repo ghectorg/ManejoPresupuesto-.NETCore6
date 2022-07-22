eb1bc747add6953258a2d59d558fc5e6e289273b
Hector Garcia <g.hector9983@gmail.com>
2022-07-22 10:52:57 -0400

    First commit

---
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,69 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        private readonly IRepositorioTiposCuentas repoTiposCuentas;
+
+        public TiposCuentasController(IRepositorioTiposCuentas repoTiposCuentas)
+        {
+            this.repoTiposCuentas = repoTiposCuentas;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var usuarioId = 1;
+            var tiposCuentas = await repoTiposCuentas.Obtener(usuarioId);
+            return View(tiposCuentas);
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+
+            tipoCuenta.UsuarioId = 1;
+
+            var yaExiste = await repoTiposCuentas.Existe(tipoCuenta.Nombre, tipoCuenta.UsuarioId);
+
+            if (yaExiste)
+            {
+                ModelState.AddModelError(nameof(tipoCuenta.Nombre), $"El nombre {tipoCuenta.Nombre} ya existe.");
+
+                return View(tipoCuenta);
+            }
+
+            await repoTiposCuentas.Crear(tipoCuenta);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> VerificarExisteTipoCuenta(string nombre)
+        {
+            var usuarioId = 1;
+            var yaExiste = await repoTiposCuentas.Existe(nombre, usuarioId);
+
+            if (yaExiste)
+            {
+                return Json("$El nombre {nombre} ya existe");
+
+            }
+
+            return Json(true);
+        }
+    }
+}
---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,49 @@
+﻿using ManejoPresupuesto.Validaciones;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta  //: IValidatableObject
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        //[StringLength(maximumLength:50, MinimumLength =3, ErrorMessage = "La longitud del campo {0} debe estar entre {2} y {1}")]
+        //[Display(Name ="Nombre del tipo cuenta")]
+        [PrimeraLetraMayus] //VALIDACION PERSONALIZADA POR ATRIBUTO
+        [Remote(action: "VerificarExisteTipoCuenta", controller: "TiposCuentas")]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
+        /* VALIDACION PERSOLAZADA POR MODELO */
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Nombre != null && Nombre.Length > 0)
+        //    {
+        //        var primeraLetra = Nombre[0].ToString();
+
+        //        if (primeraLetra != primeraLetra.ToUpper())
+        //        {
+        //            yield return new ValidationResult("La primera letra debe ser mayuscula", new[] {nameof(Nombre)});
+        //        }
+        //    }
+        //}
+
+        /* PRUEBAS DE OTRAS VALIDACIONES POR DEFECTO */
+
+        //[Required]
+        //[EmailAddress]
+        //public string Email { get; set; }
+        //[Required]
+        //[Range(minimum:18, maximum:130)]
+        //public int Edad { get; set; }
+        //[Required]
+        //[Url(ErrorMessage ="El campo debe ser una URL valida")]
+        //public string URL { get; set; }
+        //[Required]
+        //[CreditCard(ErrorMessage ="La tarjeta de credito no es valida")]
+        //public string TarjetaDeCredito { get; set; }
+    }
+}
